89aaa783718412e467b74edf9daacf2413da4d75
joseramondelgadoMACPRO <interamaster@gmail.com>
2016-10-22 13:02:05 +0200

    v0.56 cambiado listviewpokemon con animaciones

--- app/src/main/java/jrdv/mio/com/entrenatablasmultiplicacion/CustomListAdapterPokemo.java
+++ app/src/main/java/jrdv/mio/com/entrenatablasmultiplicacion/CustomListAdapterPokemo.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,9 +25,12 @@ public class CustomListAdapterPokemo extends ArrayAdapter<String> {
     //mio para pokemonsimagenes
     private final ArrayList<Integer> imagenPokemons;
 
+    //para animacion
+    private int lastPosition = -1;
+
 
     public CustomListAdapterPokemo(Activity context, String[] itemname, ArrayList<Integer> imagenPokemons) {
-        super(context, R.layout.pokemonlist, itemname);
+        super(context, R.layout.new_pokemonlist, itemname);
         // TODO Auto-generated constructor stub
 
         this.context=context;
@@ -42,17 +47,25 @@ public class CustomListAdapterPokemo extends ArrayAdapter<String> {
 
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.pokemonlist, null,true);
+        View rowView=inflater.inflate(R.layout.new_pokemonlist, null,true);
 
         TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-        TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
+       // TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
 
         txtTitle.setText(itemname[position]);
         //imageView.setImageResource(imgid[position]);
         imageView.setImageResource(imagenPokemons.get(position));
 
-        extratxt.setText("Description "+itemname[position]);
+       // extratxt.setText("Description "+itemname[position]);
+
+
+        //aqui la animacion:http://yasirameen.com/2015/09/card-style-listview-with-google-like-animation/
+
+        Animation animation = AnimationUtils.loadAnimation(getContext(), (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        rowView.startAnimation(animation);
+        lastPosition = position;
+
         return rowView;
 
     };
